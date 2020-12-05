2b8c58a1265d164538b58a0b6e008cad98b12070
KevinR117 <59045597+KevinR117@users.noreply.github.com>
2020-12-05 11:36:18 +0100

    added some buttons and play modes

--- src/app/models/play-card.model.ts
+++ src/app/models/play-card.model.ts
@@ -4,5 +4,12 @@ export enum PlayCard {
     Raise_Fold = 'handRaiseFold',
     Raise_Broke = 'handRaiseBroke',
     Limp_Fold = 'handLimpFold',
-    Limp_Call = 'handLimpCall'
+    Limp_Call = 'handLimpCall',
+    Raise_Call_Raise_Broke = "handRaiseCallRaiseBroke",
+    Raise_Fold_Raise_Broke = "handRaiseFoldRaiseBroke",
+    Raise_Call_Raise_4bet = "handRaiseCallRaise4bet",
+    Raise_Fold_Raise_4bet = "handRaiseFoldRaise4bet",
+    BVB_3bet = "handBVB3bet",
+    BVB_Call = "handBVBCall",
+    BVB_call_3bet = "handBVBCall3bet"
 }
