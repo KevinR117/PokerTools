95285445018d8715b82079b0f0c6999ae4fec239
Kevin Rives <kevin.rives@centrale-marseille.fr>
2021-03-27 13:20:20 +0100

    Added buttons

--- src/app/models/play-card.model.ts
+++ src/app/models/play-card.model.ts
@@ -5,11 +5,14 @@ export enum PlayCard {
     Raise_Broke = 'handRaiseBroke',
     Limp_Fold = 'handLimpFold',
     Limp_Call = 'handLimpCall',
-    Raise_Call_Raise_Broke = "handRaiseCallRaiseBroke",
-    Raise_Fold_Raise_Broke = "handRaiseFoldRaiseBroke",
-    Raise_Call_Raise_4bet = "handRaiseCallRaise4bet",
-    Raise_Fold_Raise_4bet = "handRaiseFoldRaise4bet",
-    BVB_3bet = "handBVB3bet",
-    BVB_Call = "handBVBCall",
-    BVB_call_3bet = "handBVBCall3bet"
+    Limp_Reraise = 'handLimpReraise',
+    Limp_Broke = 'handLimpBroke',
+    Raise_Call_Raise_Broke = 'handRaiseCallRaiseBroke',
+    Raise_Fold_Raise_Broke = 'handRaiseFoldRaiseBroke',
+    Raise_Call_Raise_4bet = 'handRaiseCallRaise4bet',
+    Raise_Fold_Raise_4bet = 'handRaiseFoldRaise4bet',
+    Raise_Fold_Raise_Call = 'handRaiseFoldRaiseCall',
+    BVB_3bet = 'handBVB3bet',
+    BVB_Call = 'handBVBCall',
+    BVB_call_3bet = 'handBVBCall3bet'
 }
